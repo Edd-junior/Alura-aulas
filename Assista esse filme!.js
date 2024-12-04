89e9b74589d6a51563212d023218ab9c068f530b
Edd <medeiroeduardohenrique@gmail.com>
2024-12-04 16:48:56 -0300

    Adicionei a checkbox de aventura e o titulo do site com o style css e a cor de fora

--- Assista esse filme!.js
+++ Assista esse filme!.js
@@ -4,36 +4,53 @@
 // paddington, LIVRE, fantasia, aventura
 // Memorias do ontem, 10, romance, drama
 // o menino que descobriu o vento, 14, drama
+// Star
+
+//Style css: 
+//margin:30px;
+//background-color: rgb();
 
 let idadeUser;
-let fantasiaUser;
+let fantasiaBox;
+let aventuraBox;
 
-function setup() {
+function setup(idadeUser,) {
   createCanvas(400, 400);
+  createElement("h2", "Assista esse filme!");
+  createSpan("Sua idade:");
   idadeUser = createImput();
-  createCheckbox("gostaDeFantasia") 
+  createCheckbox("Gosta de fantasia?");
+  createCheckbox("Gosta de aventura?") 
 }
 
-function draw() {
+function draw(idadeUser, fantasiaBox, aventuraBox) {
   background(220);
-  let idade = idadeUser.value;
-  let gostaDeFantasia = fantasiaUser.checked;
-  let recomendacao = geraRecomendacao(idade, gostaDeFantasia);
+  let idade = idadeUser.value();
+  let gostaDeFantasia = fantasiaBox.checked();
+  let gostaDeAventura = aventuraBox.checked();
+  let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
   fill(color(76, 5, 115));
   textSize(38);
   textAlign(CENTER, CENTER);
   text(recomendacao, width / 2, height / 2);
 }
 
-function geraRecomendacao(idade, gostaDeFantasia) {
+function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
   if(idade >= 10) {
     if(idade >= 14) {
       return "o menino que descobriu o vento";
     } else {
+      if(idade >= 12){
+        if(gostaDeFantasia || gostaDeAventura) {
+          return "Homem Aranha no aranha verso";
+        }
+        return "Star Wars";
+      } else {
       if(gostaDeFantasia) {
         return "as aventuras de pi";  
       } else {
         return "Memorias do ontem";
+      }
       }
     }
   }
