1973e65e633fed2725fe52a4f3201bc874e9edeb
Edd <medeiroeduardohenrique@gmail.com>
2024-12-04 14:58:17 -0300

    Atualização na lista de filmes e adição de códgod no código de texto

--- Assista esse filme!.js
+++ Assista esse filme!.js
@@ -1,12 +1,9 @@
-// fantasia, aventura, drama
-
 // a viagem de chihiro, LIVRE, fantasia, aventura
 // guardiões da galáxia, 12, fantasia, aventura
 // as aventuras de pi, 10, drama, fantasia, aventura
 // paddington, LIVRE, fantasia, aventura
-// ladrões de bicicleta, 12, drama
+// Memorias do ontem, 10, romance, drama
 // o menino que descobriu o vento, 14, drama
-// Star wars, 12, ficção científica
 
 let idadeUser;
 let fantasiaUser;
@@ -21,7 +18,10 @@ function draw() {
   background(220);
   let idade = idadeUser.value;
   let gostaDeFantasia = fantasiaUser.checked;
-  let recomendacao = geraRecomendacao(idade);
+  let recomendacao = geraRecomendacao(idade, gostaDeFantasia);
+  fill(color(125, 5, 50));
+  textSize(38);
+  textAlign(CENTER, CENTER);
   text(recomendacao, width / 2, height / 2);
 }
 
@@ -33,7 +33,7 @@ function geraRecomendacao(idade, gostaDeFantasia) {
       if(gostaDeFantasia) {
         return "as aventuras de pi";  
       } else {
-        return "Star Wars";
+        return "Memorias do ontem";
       }
     }
   }
